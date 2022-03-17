477c6de7e91ae51c22d4bdae1d383ac583a2ef77
muteebm <63216334+muteebm@users.noreply.github.com>
2022-03-18 00:14:36 +0500

    MailChimp post api implementation.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var reportRouter = require('./routes/reports');
-var sectionsRouter = require('./routes/sections')
+var sectionsRouter = require('./routes/sections');
+var chimpRouter = require('./routes/mailchimp');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/reports', reportRouter);
 app.use('/sections', sectionsRouter);
+app.use('/mailchimp', chimpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
